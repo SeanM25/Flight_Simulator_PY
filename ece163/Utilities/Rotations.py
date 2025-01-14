bb6f41691497305f4fd2274ce77dc3be447d4595
Sean Manger <smanger@ucsc.edu>
2025-01-13 20:39:12 -0800

    coded ned2enu prototype

--- ece163/Utilities/Rotations.py
+++ ece163/Utilities/Rotations.py
@@ -1,2 +1,14 @@
 import math
 from . import MatrixMath
+
+
+
+def ned2enu(points):
+
+    MTRX_NED2ENU = [[0, 1, 0], [1, 0, 0], [0, 0, -1]]
+
+    NED2ENU_FINAL = MTRX_NED2ENU * MatrixMath.transpose(points)
+
+    NED2ENU_FINAL = MatrixMath.transpose(NED2ENU_FINAL)
+
+    return NED2ENU_FINAL
