4088100617825c53365ea6f57dd69e0320ff8ae3
Sean Manger <smanger@ucsc.edu>
2025-01-13 23:52:49 -0800

    euler2DCM() coded needs further testing

--- TestHarnesses/testChapter2.py
+++ TestHarnesses/testChapter2.py
@@ -33,6 +33,11 @@ assert(compareVectors([[0], [0], [-1]],[[1e-13], [0], [-1+1e-9]]))
 assert(not compareVectors([[0], [0], [-1]],[[1e-11], [0], [-1]]))
 assert(not compareVectors([[1e8], [0], [-1]],[[1e8+1], [0], [-1]]))
 
+def compareDCM(a, b):
+	"""A quick tool to compare two DCM's"""
+	DCM_close = [isclose(a[i][j], b[i][j]) for i in range(2) for j in range(2)]
+	return all(DCM_close)
+
 
 
 failed = []
@@ -41,14 +46,14 @@ def evaluateTest(test_name, boolean):
 	"""evaluateTest prints the output of a test and adds it to one of two 
 	global lists, passed and failed, which can be printed later"""
 	if boolean:
-		print(f"   passed {test_name}")
+		#print(f"   passed {test_name}")
 		passed.append(test_name)
 	else:
-		print(f"   failed {test_name}")
+		#print(f"   failed {test_name}")
 		failed.append(test_name)
 	return boolean
 
-
+"""
 #%% Euler2dcm():
 print("Beginning testing of Rotations.Euler2dcm()")
 
@@ -60,9 +65,62 @@ expected_vec = [[0],[-1],[0]]
 actual_vec = mm.multiply(R, orig_vec)
 if not evaluateTest(cur_test, compareVectors(expected_vec, actual_vec) ):
 	print(f"{expected_vec} != {actual_vec}")
+"""
+
+#%% ned2enu(): 
+
+print("Beginning testing of Rotations.ned2enu()")
+
+ned_points_T1 = [[1, 2, 3],
+			 [4, 5, 6]]
+
+ned_points_T2 = [[5, 2, 7],
+			 [4, 3, 8],
+			 [7, 3, 2]]
+
+ned_points_T3 = [[2, 4, 6]]
+
+ned_T1_expect = [[2, 1, -3], 
+			 [5, 4, -6]]
+
+ned_T2_expect = [[2, 5, -7],
+			 [3, 4, -8],
+			 [3, 7, -2]]
+
+ned_T3_expect = [[4, 2, -6]]
+
+ned_T1_act = Rotations.ned2enu(ned_points_T1)
+
+ned_T2_act = Rotations.ned2enu(ned_points_T2)
+
+ned_T3_act = Rotations.ned2enu(ned_points_T3)
+
+cur_TEST_NED = "Test 1 ned2enu()"
+
+evaluateTest(cur_TEST_NED, ned_T1_expect == ned_T1_act)
+
+cur_TEST_NED = "Test 2 ned2enu()"
+
+evaluateTest(cur_TEST_NED, ned_T2_expect == ned_T2_act)
+
+cur_TEST_NED = "Test 3 ned2enu()"
+
+evaluateTest(cur_TEST_NED, ned_T3_expect == ned_T3_act)
 
 
-#%%  
+#%% euler2DCM(): 
+
+print("Beginning testing of Rotations.euler2DCM():")
+
+e2DCM_T1_expect = [[(-math.sqrt(2) / 4), (-math.sqrt(2) / 4), (-math.sqrt(3) / 2)], [((1/2) - (math.sqrt(3) / 4)), ((-1/2) - (math.sqrt(3) / 4)), (-math.sqrt(2) / 4)], [((-1/2) - (math.sqrt(3) / 4)), ((1/2) - (math.sqrt(3) / 4)), (math.sqrt(2) / 4)]] # Given as example in HW 0
+
+e2DCM_T1_act = Rotations.euler2DCM(-135*(math.pi / 180), 60*(math.pi / 180), 45*(math.pi / 180)) # Given as example in HW 0
+
+cur_TEST_e2DCM = "Test 1 euler2DCM()"
+
+evaluateTest(cur_TEST_e2DCM, compareDCM(e2DCM_T1_act,e2DCM_T1_expect))
+
+cur_TEST_e2DCM = "Test 2 euler2DCM()"
 
 """
 Students, add more tests here.  
--- ece163/Utilities/Rotations.py
+++ ece163/Utilities/Rotations.py
@@ -5,16 +5,25 @@ from . import MatrixMath
 
 def ned2enu(points):
 
-    MTRX_NED2ENU = [[0, 1, 0], [1, 0, 0], [0, 0, -1]]
+    MTRX_TO_ENU = [[0, 1, 0], [1, 0, 0], [0, 0, -1]]
 
-    NED2ENU_FINAL = MTRX_NED2ENU * MatrixMath.transpose(points)
+    result = MatrixMath.multiply(MTRX_TO_ENU, MatrixMath.transpose(points))
 
-    NED2ENU_FINAL = MatrixMath.transpose(NED2ENU_FINAL)
+    result = MatrixMath.transpose(result)
 
-    return NED2ENU_FINAL
+    return result
 
 def euler2DCM (yaw, pitch, roll):
 
+    
+    DCM = [[(math.cos(pitch) * math.cos(yaw)), (math.cos(pitch) * math.sin(yaw)), (-1*math.sin(pitch))],
+           [((math.sin(roll) * math.sin(pitch) * math.cos(yaw)) - (math.cos(roll) * math.sin(yaw))), ((math.sin(roll) * math.sin(pitch) * math.sin(yaw)) + (math.cos(roll) * math.cos(yaw))), (math.sin(roll) * math.cos(pitch))],
+           [((math.cos(roll) * math.sin(pitch) * math.cos(yaw)) + (math.sin(roll) * math.sin(yaw))), ((math.cos(roll) * math.sin(pitch) * math.sin(yaw)) - (math.sin(roll) * math.cos(yaw))), (math.cos(roll) * math.cos(pitch))]]
 
-    return 0
+    #DCM = math.cos(yaw) * math.cos(pitch) * math.cos(roll)
+
+    #MatrixMath.matrixPrint(DCM)
+
+
+    return DCM
 
