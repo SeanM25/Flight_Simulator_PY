bc9d0352511f16b139771cec4ddc69cf299bdfdd
Sean Manger <smanger@ucsc.edu>
2025-01-25 11:59:48 -0800

    VehicleDynamicsModel class defined and intialized

--- ece163/Modeling/VehicleDynamicsModel.py
+++ ece163/Modeling/VehicleDynamicsModel.py
@@ -3,3 +3,26 @@ from ..Containers import States
 from ..Utilities import MatrixMath
 from ..Utilities import Rotations
 from ..Constants import VehiclePhysicalConstants as VPC
+
+
+
+class VehicleDynamicsModel:
+
+
+    def __init__(self, dT = VPC.dT):
+
+        '''Initializes the class, and sets the time step (needed for Rexp and integration). Instantiates attributes for vehicle state, and time derivative of vehicle state.'''
+
+        self.dT = dT # Assign time step as the given dT in VPC
+
+        self.state = States.vehicleState() # Instantiates state as an instance of States.vehicleState()
+
+        self.dot = States.vehicleState() # Instantiates dot (time derivative) as an instance of States.vehicleState()
+
+        return
+
+
+
+
+
+
