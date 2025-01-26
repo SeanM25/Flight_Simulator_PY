5bbf29aef0cc8834593da67edda66a66b0cdaabc
Sean Manger <smanger@ucsc.edu>
2025-01-26 12:46:14 -0800

    ...

--- ece163/Modeling/VehicleDynamicsModel.py
+++ ece163/Modeling/VehicleDynamicsModel.py
@@ -162,9 +162,9 @@ class VehicleDynamicsModel:
         
         dot_YPR = mm.multiply(YPR_dir_mtrx, yaw_pitch_roll) # get yaw pitch and roll derivatives
 
-        roll_dot = dot_YPR[0][0] # Get roll dot
+        roll_dot = dot_YPR[1][0] # Get roll dot
 
-        pitch_dot = dot_YPR[1][0] # Get pitch dot
+        pitch_dot = dot_YPR[0][0] # Get pitch dot
         
         yaw_dot = dot_YPR[2][0] # get yaw dot
 
