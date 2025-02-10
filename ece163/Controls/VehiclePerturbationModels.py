1bf729a0fa539c407dbcde68c9733caccc22efdb
Sean Manger <smanger@ucsc.edu>
2025-02-10 03:46:02 -0800

    ...

--- ece163/Controls/VehiclePerturbationModels.py
+++ ece163/Controls/VehiclePerturbationModels.py
@@ -6,7 +6,7 @@ from ece163.Containers import Inputs
 from ece163.Containers import Linearized
 from ece163.Utilities import MatrixMath
 
-def dThrust_dVa(self, Va, Throttle, epsilon=0.5):
+def dThrust_dVa(Va, Throttle, epsilon=0.5):
 
     '''
     Function to calculate the numerical partial derivative of propeller thrust to change in airspeed using the actual prop function
@@ -17,18 +17,18 @@ def dThrust_dVa(self, Va, Throttle, epsilon=0.5):
     # This function gets the partial derivative of thrust with respect to the propeller this is easily accomplished via the definition from Calc
     # Essentially we add epsilon to the parameter we are taking the derivative with respect to in this case Va
 
-    Fx , Mx = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, Va, Throttle) # Regular Fx Mx not used
+    Fx , Mx = VAM.VehicleAerodynamicsModel().CalculatePropForces(Va, Throttle) # Regular Fx Mx not used
 
     nV = Va + epsilon
 
-    Fx_eps, mx_eps = VAM.VehicleAerodynamicsModel.CalculatePropForces(nV, Throttle) # Fx with epsilon added to Va
+    Fx_eps, mx_eps = VAM.VehicleAerodynamicsModel().CalculatePropForces(nV, Throttle) # Fx with epsilon added to Va
 
     dTdVA = (Fx_eps - Fx) / epsilon # Get partial derivative w/ the formula
 
     return dTdVA # Return partial of Thrust with respect to Va
 
 
-def dThrust_dThrottle(self, Va, Throttle, epsilon=0.01):
+def dThrust_dThrottle(Va, Throttle, epsilon=0.01):
 
 
     '''
@@ -38,11 +38,11 @@ def dThrust_dThrottle(self, Va, Throttle, epsilon=0.01):
 
     # Same Idea as above different partial this time with respect to throttle
 
-    Fx, Mx = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, Va, Throttle) # Regular Fx
+    Fx, Mx = VAM.VehicleAerodynamicsModel().CalculatePropForces(Va, Throttle) # Regular Fx
 
     nT = Throttle + epsilon
 
-    Fx_eps, Mx_eps = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, Va, nT) # Epsilon added to throttle
+    Fx_eps, Mx_eps = VAM.VehicleAerodynamicsModel().CalculatePropForces(Va, nT) # Epsilon added to throttle
 
     dTdT = (Fx_eps - Fx) / epsilon # Get the partial using the formula
 
