56e2074f1c5d97759e78f3397cfa843765439f53
Sean Manger <smanger@ucsc.edu>
2025-02-10 02:51:04 -0800

    coded partial with respect to throttle

--- ece163/Controls/VehiclePerturbationModels.py
+++ ece163/Controls/VehiclePerturbationModels.py
@@ -23,6 +23,26 @@ def dThrust_dVa(self, Va, Throttle, epsilon=0.5):
 
     dTdVA = (Fx_eps - Fx) / epsilon # Get partial derivative w/ the formula
 
-    return dTdVA # Return partial of Thrus with respect to Va
+    return dTdVA # Return partial of Thrust with respect to Va
+
+
+def dThrust_dThrottle(self, Va, Throttle, epsilon=0.01):
+
+
+    '''
+    Function to calculate the numerical partial derivative of propeller thrust to change
+    in throttle setting using the actual prop function from complex propeller model (inside the VehicleAerodynamicsModel class)
+    '''
+
+    # Same Idea as above different partial this time with respect to throttle
+
+    Fx, Mx = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, Va, Throttle) # Regular Fx
+
+    Fx_eps, Mx_eps = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, Va, (Throttle + epsilon)) # Epsilon added to throttle
+
+    dTdT = (Fx_eps - Fx) / epsilon # Get the partial using the formula
+
+    return dTdT # Return partial of Thrust with respect to Throttle
+    
 
 
