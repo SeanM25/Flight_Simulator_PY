86301f9402af43f4734620e48b4ea7721a3e59e5
Sean Manger <smanger@ucsc.edu>
2025-02-10 03:19:40 -0800

    ...

--- ece163/Controls/VehiclePerturbationModels.py
+++ ece163/Controls/VehiclePerturbationModels.py
@@ -6,7 +6,7 @@ from ece163.Containers import Inputs
 from ece163.Containers import Linearized
 from ece163.Utilities import MatrixMath
 
-def dThrust_dVa(self, Va, Throttle, epsilon=0.5):
+def dThrust_dVa(Va, Throttle, epsilon=0.5):
 
     '''
     Function to calculate the numerical partial derivative of propeller thrust to change in airspeed using the actual prop function
@@ -17,16 +17,16 @@ def dThrust_dVa(self, Va, Throttle, epsilon=0.5):
     # This function gets the partial derivative of thrust with respect to the propeller this is easily accomplished via the definition from Calc
     # Essentially we add epsilon to the parameter we are taking the derivative with respect to in this case Va
 
-    Fx , Mx = VAM.VehicleAerodynamicsModel.CalculatePropForces(self,Va,Throttle) # Regular Fx Mx not used
+    Fx , Mx = VAM.VehicleAerodynamicsModel.CalculatePropForces(Va,Throttle) # Regular Fx Mx not used
 
-    Fx_eps, mx_eps = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, (Va + epsilon), Throttle) # Fx with epsilon added to Va
+    Fx_eps, mx_eps = VAM.VehicleAerodynamicsModel.CalculatePropForces((Va + epsilon), Throttle) # Fx with epsilon added to Va
 
     dTdVA = (Fx_eps - Fx) / epsilon # Get partial derivative w/ the formula
 
     return dTdVA # Return partial of Thrust with respect to Va
 
 
-def dThrust_dThrottle(self, Va, Throttle, epsilon=0.01):
+def dThrust_dThrottle(Va, Throttle, epsilon=0.01):
 
 
     '''
@@ -36,13 +36,29 @@ def dThrust_dThrottle(self, Va, Throttle, epsilon=0.01):
 
     # Same Idea as above different partial this time with respect to throttle
 
-    Fx, Mx = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, Va, Throttle) # Regular Fx
+    Fx, Mx = VAM.VehicleAerodynamicsModel.CalculatePropForces(Va, Throttle) # Regular Fx
 
-    Fx_eps, Mx_eps = VAM.VehicleAerodynamicsModel.CalculatePropForces(self, Va, (Throttle + epsilon)) # Epsilon added to throttle
+    Fx_eps, Mx_eps = VAM.VehicleAerodynamicsModel.CalculatePropForces(Va, (Throttle + epsilon)) # Epsilon added to throttle
 
     dTdT = (Fx_eps - Fx) / epsilon # Get the partial using the formula
 
     return dTdT # Return partial of Thrust with respect to Throttle
+
+
+def CreateTransferFunction(trimState, trimInputs):
+
+    '''
+    Function to fill the transfer function parameters used for the successive loop closure from the given trim state and trim inputs. 
+    Note that these parameters will be later used to generate actual control loops.
+    Vehicle Perturbation models are developed using the trim state and inputs. 
+    Models for transfer function parameters and state space implementations are calculated using constants in VehiclePhysicalParameters and the input trim state and trim control inputs.
+    Results are returned as a Linearized.transferFunction class.
+    '''
     
+    # Initialize TF object
+
+    TransFunct = Linearized.transferFunctions()
+
+    # Get all constants needed for the Transfer Function
 
 
