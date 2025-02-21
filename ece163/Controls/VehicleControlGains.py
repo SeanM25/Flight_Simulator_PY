2632a0c6ff6426621aba107cb024ff5448e782b4
Sean Manger <smanger@ucsc.edu>
2025-02-20 23:52:08 -0800

    Coded PD control Update

--- ece163/Controls/VehicleControlGains.py
+++ ece163/Controls/VehicleControlGains.py
@@ -31,11 +31,43 @@ class PDControl(kp=0.0, kd=0.0, trim=0.0, lowLimit=0.0, highLimit=0.0):
 
         self.kd = kd # set derivative gain to given deriv gain
 
-        self.trim = trim # set trim output to given trim output
+        self.trim = trim # set trim output to given trim output (added the the loop computed output)
 
         self.lowLimit = lowLimit # set lower limit saturation
 
         self.highLimit = highLimit # set upper limit of saturation
 
         return # Return nothing
+    
+    def Update(self, command=0.0, current=0.0, derivative=0.0):
+
+        '''Calculates the output of the PD loop given the gains and limits from instantiation, and using the command, actual, and derivative inputs. Output is limited to between lowLimit and highLimit from instantiation.'''
+
+    # Parameters:
+
+        # command: Reference input command
+
+        # current: Actual output or sensor
+
+        # derivative: output or sensor derivative
+
+        error = command - current # get error which is the input command minus the output actual
+
+        u = self.trim + (self.kp * error) - (self.kd * derivative) # Given equation with trim added in as u_ref
+
+        # Check for saturation:
+
+        if (u > self.highLimit): # If u is greater than the upper saturation limit then set it to the upper limit
+
+            u = self.highLimit
+
+        elif (u < self.lowLimit): # If u is smaller than the lower saturation limit then set it to the lower limit
+
+            u = self.lowLimit
+
+
+        return u # Return PD control output
+    
+
+    
 
