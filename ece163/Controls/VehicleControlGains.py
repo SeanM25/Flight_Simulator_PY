68a275da3b738f413ed9cf95a085588cd087557d
Sean Manger <smanger@ucsc.edu>
2025-02-20 23:11:05 -0800

    Coded init for PDControl

--- ece163/Controls/VehicleControlGains.py
+++ ece163/Controls/VehicleControlGains.py
@@ -8,3 +8,34 @@ from ece163.Containers import Controls
 from ece163.Containers import Linearized
 from ece163.Utilities import MatrixMath
 from ece163.Utilities import Rotations
+
+
+
+class PDControl(kp=0.0, kd=0.0, trim=0.0, lowLimit=0.0, highLimit=0.0):
+
+    '''Functions which implement the PD control with saturation where the derivative is available as a separate input to the function. The output is: u = u_ref + Kp * error - Kd * dot{error} limited between lowLimit and highLimit.'''
+
+    def __init__(self, kp=0.0, kd=0.0, trim=0.0, lowLimit=0.0, highLimit=0.0):
+        
+    # Here we set the intial conditions for the Proportional Derivative controller to the given values which default to 0
+
+    # Paramaterss:
+
+        # kp: proportional gain
+        # kd : derivative gain
+        # trim : trim output
+        # lowLimit : lower limit of accumulator saturation
+        # highLimit : upper limit of accumulator saturation
+
+        self.kp = kp # set proprtional gain to given proprtional gain
+
+        self.kd = kd # set derivative gain to given deriv gain
+
+        self.trim = trim # set trim output to given trim output
+
+        self.lowLimit = lowLimit # set lower limit saturation
+
+        self.highLimit = highLimit # set upper limit of saturation
+
+        return # Return nothing
+
