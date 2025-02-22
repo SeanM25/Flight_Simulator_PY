be3c0ac05c7b911460c79dfd23083edff2b9825d
Sean Manger <smanger@ucsc.edu>
2025-02-21 21:52:58 -0800

    Added and partually coded test harness fixed class typo in PD control

---
+++ TestHarnesses/testChapter6.py
@@ -0,0 +1,109 @@
+"""This file is a test harness for the module VehiclePerturbationModels. 
+
+It is meant to be run from the Testharnesses directory of the repo with:
+
+python ./TestHarnesses/testChapter5.py (from the root directory) -or-
+python testChapter5.py (from inside the TestHarnesses directory)
+
+at which point it will execute various tests on the VehiclePerturbationModels module"""
+
+#%% Initialization of test harness and helpers:
+
+import math
+
+import sys
+sys.path.append("..") #python is horrible, no?
+
+import ece163.Utilities.MatrixMath as mm
+import ece163.Utilities.Rotations as Rotations
+import ece163.Modeling.VehicleDynamicsModel as VDM
+import ece163.Controls.VehiclePerturbationModels as VPM
+import ece163.Modeling.WindModel as WM
+#import ece163.Controls.VehicleTrim as VehicleTrim
+import ece163.Containers.Inputs as Inputs
+import ece163.Containers.States as States
+import ece163.Controls.VehicleClosedLoopControl as VCLC
+
+"""math.isclose doesn't work well for comparing things near 0 unless we 
+use an absolute tolerance, so we make our own isclose:"""
+isclose = lambda  a,b : math.isclose(a, b, abs_tol= 1e-12)
+
+def compareVectors(a, b):
+	"""A quick tool to compare two vectors"""
+	el_close = [isclose(a[i][0], b[i][0]) for i in range(3)]
+	return all(el_close)
+
+#of course, you should test your testing tools too:
+assert(compareVectors([[0], [0], [-1]],[[1e-13], [0], [-1+1e-9]]))
+assert(not compareVectors([[0], [0], [-1]],[[1e-11], [0], [-1]]))
+assert(not compareVectors([[1e8], [0], [-1]],[[1e8+1], [0], [-1]]))
+
+
+
+failed = []
+passed = []
+def evaluateTest(test_name, boolean):
+	"""evaluateTest prints the output of a test and adds it to one of two 
+	global lists, passed and failed, which can be printed later"""
+	if boolean:
+		print(f"   passed {test_name}")
+		passed.append(test_name)
+	else:
+		print(f"   failed {test_name}")
+		failed.append(test_name)
+	return boolean
+
+
+#%% PUT A TEST HERE?
+
+# PD Control Tests
+
+PD_Init = VCLC.PDControl(1, 1, 1, 2, 4)
+
+cur_test_PD = "PD Control Initialization Test"
+
+if(PD_Init.kp == 1 and PD_Init.kd == 1 and PD_Init.trim == 1 and PD_Init.lowLimit == 2 and PD_Init.highLimit == 4):
+	
+    PD_good = True
+	
+evaluateTest(cur_test_PD, PD_good)
+
+cur_test_PD = "PD Update Test"
+
+u_act = VCLC.PDControl.Update(PD_Init, 8, 2, 0)
+
+u_expect = 4
+
+if(isclose(u_act,u_expect)):
+	
+    Update_pass = True
+	
+evaluateTest(cur_test_PD, Update_pass)    
+
+
+
+cur_test_PD = "setPDGains Test"
+
+VCLC.PDControl.setPDGains(PD_Init, 1, 2, 3, 4, 5)
+
+if(PD_Init.kp == 1 and PD_Init.kd == 2 and PD_Init.trim == 3 and PD_Init.lowLimit == 4 and PD_Init.highLimit == 5):
+	
+    PD_good_again = True
+	
+evaluateTest(cur_test_PD, PD_good_again)
+
+# PI Tests
+
+
+
+
+
+#%% Print results:
+
+total = len(passed) + len(failed)
+print(f"\n---\nPassed {len(passed)}/{total} tests")
+[print("   " + test) for test in passed]
+
+if failed:
+	print(f"Failed {len(failed)}/{total} tests:")
+	[print("   " + test) for test in failed]
--- ece163/Controls/VehicleClosedLoopControl.py
+++ ece163/Controls/VehicleClosedLoopControl.py
@@ -6,3 +6,92 @@ import ece163.Constants.VehiclePhysicalConstants as VPC
 import ece163.Modeling.VehicleAerodynamicsModel as VehicleAerodynamicsModule
 import ece163.Controls.VehicleEstimator as VehicleEstimator
 import ece163.Sensors.SensorsModel as SensorsModel
+
+
+class PDControl:
+
+    '''Functions which implement the PD control with saturation where the derivative is available as a separate input to the function. The output is: u = u_ref + Kp * error - Kd * dot{error} limited between lowLimit and highLimit.'''
+
+    def __init__(self, kp=0.0, kd=0.0, trim=0.0, lowLimit=0.0, highLimit=0.0):
+        
+    # Here we set the intial conditions for the Proportional Derivative controller to the given values which default to 0
+
+    # Paramaterss:
+
+        # kp: proportional gain
+        # kd : derivative gain
+        # trim : trim output
+        # lowLimit : lower limit of accumulator saturation
+        # highLimit : upper limit of accumulator saturation
+
+        self.kp = kp # set proprtional gain to given proprtional gain
+
+        self.kd = kd # set derivative gain to given deriv gain
+
+        self.trim = trim # set trim output to given trim output (added the the loop computed output)
+
+        self.lowLimit = lowLimit # set lower limit saturation
+
+        self.highLimit = highLimit # set upper limit of saturation
+
+        return # Return nothing
+    
+    def Update(self, command=0.0, current=0.0, derivative=0.0):
+
+        '''Calculates the output of the PD loop given the gains and limits from instantiation, and using the command, actual, and derivative inputs. Output is limited to between lowLimit and highLimit from instantiation.'''
+
+    # Parameters:
+
+        # command: Reference input command
+
+        # current: Actual output or sensor
+
+        # derivative: output or sensor derivative
+
+        error = command - current # get error which is the input command minus the output actual
+
+        u = self.trim + (self.kp * error) - (self.kd * derivative) # Given equation with trim added in as u_ref
+
+        # Check for saturation:
+
+        if (u > self.highLimit): # If u is greater than the upper saturation limit then set it to the upper limit
+
+            u = self.highLimit
+
+        elif (u < self.lowLimit): # If u is smaller than the lower saturation limit then set it to the lower limit
+
+            u = self.lowLimit
+
+
+        return u # Return PD control output
+    
+
+    
+    def setPDGains(self, kp=0.0, kd=0.0, trim=0.0, lowLimit=0.0, highLimit=0.0):
+
+        '''Function to set the gains for the PD control block (including the trim output and the limits)'''
+
+    # Basically this function is kinda like init but rather than initializing we are changing values on the fly
+
+        # Parameters:
+            # kp: proportional gain
+            # kd : derivative gain
+            # trim : trim output
+            # lowLimit : lower limit of accumulator saturation
+            # highLimit : upper limit of accumulator saturation 
+
+        self.kp = kp # kp is new Kp
+
+        self.kd = kd # kd is new kd
+
+        self.trim = trim # trim is new trim
+
+        self.lowLimit = lowLimit # lowLimit is new lowLimit
+
+        self.highLimit = highLimit # highLimit is new highLimit
+
+        return # return nothing
+    
+#class PIControl:
+
+    #def __init__(self, dT=VPC.dT, kp=0.0, ki=0.0, trim=0.0, lowLimit=0.0, highLimit=0.0):
