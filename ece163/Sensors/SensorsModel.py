b1fe0766a8e518fcca4404a522442ceedf3c52c7
Sean Manger <smanger@ucsc.edu>
2025-03-02 22:48:14 -0800

    coded GM reset

--- ece163/Sensors/SensorsModel.py
+++ ece163/Sensors/SensorsModel.py
@@ -13,6 +13,8 @@ class GaussMarkov:
 
     def __init__(self, dT = 0.01, tau = 1.0e6, eta = 0.0):
 
+        # Sets Inital Parameters to Compute the Gauss Markov Model
+
 
         self.tau = tau # Get tau parameter
 
@@ -20,12 +22,26 @@ class GaussMarkov:
 
         self.dT = dT # get time step
 
-        self.V = 0.0 # Current GM Value is set to 0
-
-        self.prev_V  = 0.0 # Prev GM state is also 0
+        self.prev_V  = 0.0 # Prev GM state is intitialized to 0
 
         return # Return nothing
     
+
+    def reset(self):
+
+        # Resets All Gauss Markov Parameters to IC's
+
+        self.dT = 0.01 # reset time step
+
+        self.tau = 1.0e6 # Reset tau
+
+        self.eta = 0.0 # Reset eta
+
+        self.prev_V = 0.0 # Reset prev GM state
+
+        return # return nothing
+
+        
 
     
         
