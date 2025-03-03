bb2219c38e3bb905fc0c9a9b714d0c188700b4b1
Sean Manger <smanger@ucsc.edu>
2025-03-02 20:19:50 -0800

    Coded Gauss Markov Init

--- ece163/Sensors/SensorsModel.py
+++ ece163/Sensors/SensorsModel.py
@@ -6,3 +6,34 @@ from ..Containers import Sensors
 from ..Constants import VehiclePhysicalConstants as VPC
 from ..Constants import VehicleSensorConstants as VSC
 from ..Modeling import VehicleAerodynamicsModel
+
+
+
+class GaussMarkov:
+
+    def __init__(self, dT = 0.01, tau = 1.0e6, eta = 0.0):
+
+
+        self.tau = tau # Get tau parameter
+
+        self.eta = eta # Get eta
+
+        self.dT = dT # get time step
+
+        self.V = 0.0 # Current GM Value is set to 0
+
+        self.prev_V  = 0.0 # Prev GM state is also 0
+
+        return # Return nothing
+    
+
+    
+        
+
+
+        
+
+
+
+
+        
